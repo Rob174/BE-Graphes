3c5ddd589133d14bc511bade37fab727c6577cf3
Ro Mo <robin.128.456@gmail.com>
2020-03-30 09:15:06 +0200

    Première implémentation de Dijkstra étape 3.D

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
@@ -1,5 +1,12 @@
 package org.insa.graphs.algorithm.shortestpath;
 
+import java.util.Arrays;
+
+import org.insa.graphs.model.Arc;
+import org.insa.graphs.model.Graph;
+import org.insa.graphs.algorithm.shortestpath.Label;
+import org.insa.graphs.algorithm.utils.*;
+
 public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
     public DijkstraAlgorithm(ShortestPathData data) {
@@ -9,8 +16,41 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
     @Override
     protected ShortestPathSolution doRun() {
         final ShortestPathData data = getInputData();
+        Graph graph = data.getGraph();
         ShortestPathSolution solution = null;
-        // TODO:
+        final int nbNodes = graph.size();
+        BinaryHeap<Label> tas = new BinaryHeap<Label>();
+        
+        // Initialize array of distances.
+        Label[] labels = new Label[nbNodes];
+        for (int i = 0; i < nbNodes; i++) {
+        	if(i==data.getOrigin().getId()) {
+    			labels[i] = new Label(graph.getNodes().get(i),false,0,null);
+    			tas.insert(labels[i]);
+        	}
+        	else {
+    			labels[i] = new Label(graph.getNodes().get(i),false,Double.POSITIVE_INFINITY,null);
+        	}
+		}
+
+        while (labels[data.getDestination().getId()].estMarque() == false) {
+        	Label x = tas.findMin();
+        	x.marquer();
+        	for (Arc arc : x.getCurrentNode().getSuccessors()) {
+        		Label successeur = labels[arc.getDestination().getId()];
+				if(successeur.estMarque()==false && successeur.getCost() > x.getCost()+arc.getMinimumTravelTime()) {
+					successeur.setCost(Math.min(successeur.getCost(), x.getCost()+arc.getMinimumTravelTime()));
+					if(successeur.getCost() != Double.POSITIVE_INFINITY) {
+						tas.remove(successeur);
+						tas.insert(successeur);
+					}
+					else {
+						tas.insert(successeur);
+					}
+				}
+			}
+        }
+        
         return solution;
     }
 
--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/Label.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/Label.java
@@ -2,7 +2,7 @@ package org.insa.graphs.algorithm.shortestpath;
 
 import org.insa.graphs.model.Node;
 
-public class Label {
+public class Label implements Comparable<Label>{
     private Node sommet_courant;
     private boolean marque;
     private double cout;
@@ -17,5 +17,27 @@ public class Label {
     }
     public double getCost() {
         return this.cout;
+    }
+    public void setCost(double cout) {
+    	this.cout = cout;
+    }
+    public boolean estMarque() {
+    	return this.marque;
+    }
+    public void marquer() {
+    	this.marque = true;
+    }
+    public Node getCurrentNode() {
+    	return this.sommet_courant;
+    }
+    public void setFather(Node n) {
+    	this.pere = n;
+    }
+    public int compareTo(Label otherLabel) {
+    	if(this.cout < otherLabel.getCost())
+    		return -1;
+    	else if(this.cout > otherLabel.getCost())
+    		return 1;
+		return 0;
     }
 }

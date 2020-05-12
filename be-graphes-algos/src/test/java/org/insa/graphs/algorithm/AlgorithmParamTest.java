dcbe24558338e6e6e67c0ac4d273c720e261502d
Ro Mo <robin.128.456@gmail.com>
2020-05-13 01:19:25 +0200

    Echec des tests JUnit sur la carte haute-garonne.
    Les autres tests passent\n\Bug : https://github.com/Rob174/BE-Graphes/issues/

--- be-graphes-algos/src/test/java/org/insa/graphs/algorithm/AlgorithmParamTest.java
+++ be-graphes-algos/src/test/java/org/insa/graphs/algorithm/AlgorithmParamTest.java
@@ -18,7 +18,7 @@ public class AlgorithmParamTest extends AlgorithmTest {
     private static ArrayList<Graph> graph_map;
     private static String[] graphs_names = new String[] { "carre", "carre-dense", "insa", "haute-garonne" };
 
-    @Parameters(name = "{index}: Carte {0}, de {1} vers {2} au plus court {3} avec {4}")
+    @Parameters(name = "{index}: Carte {0}, de {1} vers {2}")
     public static Collection<Object[]> data() {
         graph_map = new ArrayList<Graph>();
         for (String name : graphs_names) {
@@ -28,7 +28,10 @@ public class AlgorithmParamTest extends AlgorithmTest {
         return Arrays.asList(new Object[][] { { 0, 5, 4 }, // Carte carrée
                 { 1, 259, 814 }, // Carte insa
                 { 1, 1052, 656 }, { 2, 55947, 100306 }, // Carte haute-garonne
-                { 2, 153997, 101286 }, { 2, 55947, 56506 }, { 2, 60630, 144268 }, { 3, 29816, 14715 }, // Carte
+                { 2, 153997, 101286 }, 
+                { 2, 55947, 56506 }, 
+                { 2, 60630, 144268 }, 
+                { 3, 29816, 14715 }, // Carte
                                                                                                        // guadeloupe
                 { 3, 8942, 12531 }, { 3, 32670, 33072 }, { 3, 33068, 27536 }, { 3, 26278, 27536 } });
     }
